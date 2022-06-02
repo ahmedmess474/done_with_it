601db805bb437f7e9dfcf6c2d0b6556b75d92977
unknown <ahmedmess474@gmail.com>
2022-06-02 17:36:32 +0100

    fundemantel components

--- App.js
+++ App.js
@@ -1,12 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Platform,Alert,SafeAreaView, Image, Button,TouchableNativeFeedback,TouchableHighlight,TouchableWithoutFeedback, TouchableOpacity } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text>
+        Hello Ahmed!
+      </Text>
+      <TouchableNativeFeedback onPress={() => console.log("image clicked")}>
+        <Image
+            source={{
+              uri: 'https://reactnative.dev/docs/assets/p_cat2.png',
+            }}
+            fadeDuration={5000}
+            style={{ width: 200, height: 200 }}
+          />
+      </TouchableNativeFeedback>
+      <Image source={require("./assets/icon.png")} style={{ width: 200, height: 200 }} />
+      <Button title="Alert!" onPress={() => Alert.alert("My title", "My Message", [
+        {
+          text:"Yes",
+          onPress: () => console.log("yes")
+        },
+        {
+          text: "No",
+          onPress: () => console.log("no")
+        }
+      ])}/>
+    </SafeAreaView>
   );
 }
 
@@ -18,3 +39,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+
